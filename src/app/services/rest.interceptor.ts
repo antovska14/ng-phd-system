adff8900c77bb1cd5b58730ca8c9e9c4c5ec1ee6
antovska14 <antovska14@gmail.com>
2020-07-08 03:32:48 +0300

    Update routes, interceptor modifications

--- src/app/services/rest.interceptor.ts
+++ src/app/services/rest.interceptor.ts
@@ -1,19 +1,10 @@
-import {
-    HttpInterceptor,
-    HttpRequest,
-    HttpHandler,
-    HttpEvent,
-    HTTP_INTERCEPTORS,
-    HttpErrorResponse,
-    HttpEventType,
-    HttpResponse,
-} from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS, HttpErrorResponse } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
 import { Injectable, NgModule } from '@angular/core';
-import { Observable, empty } from 'rxjs';
+import { Observable, empty, throwError } from 'rxjs';
 
 import { ServiceInjector } from '../classes';
 import { AuthService } from './auth.service';
-import { catchError, tap } from 'rxjs/operators';
 
 @Injectable()
 export class RestInterceptor implements HttpInterceptor {
@@ -21,16 +12,14 @@ export class RestInterceptor implements HttpInterceptor {
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         const interceptedRequest = request.clone();
         return next.handle(interceptedRequest).pipe(
-            tap((event: HttpEvent<any>) => {
-                console.log(event instanceof HttpResponse);
-            }),
             catchError((error: HttpErrorResponse) => {
                 switch (error.status) {
                     case 401:
                         this.handle401Error();
-                        return empty();
+                        return throwError(error);
                     default:
                         this.handleUnknownError();
+                        return throwError(error);
                 }
             })
         );
--- src/app/shared/routes.module.ts
+++ src/app/shared/routes.module.ts
@@ -1,24 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { RoutePath } from '../enums';
+import { AuthGuard } from '../services/guards/auth.guard';
+
 import { SetPasswordPageComponent } from '../pages/set-password/set-password-page.component';
 import { LoginPageComponent } from '../pages/login/login-page.component';
-import { DashboardComponent } from '../pages/dashboard/dashboard.component';
+
 import { LayoutPageComponent } from '../pages/layout/layout-page.component';
+import { DashboardComponent } from '../pages/dashboard/dashboard.component';
 
 import { StudentMainPageComponent } from '../components/student/student-main-page/student-main-page.component';
 import { StudentDetailPageComponent } from '../components/student/student-details-page/student-details-page.component';
 import { AddStudentPageComponent } from '../components/student/add-student-page/add-student-page.component';
 import { StudentFilesComponent } from '../components/student/student-files/student-files.component';
-import { ProfessionalFieldMainPageComponent } from '../components/professional-field/professional-field-main-page/professional-field-main-page.component';
 import { ExamsComponent } from '../components/student/exams/exams.component';
-import { PhdProgramMainPageComponent } from '../components/phd-program/phd-program-main-page/phd-program-main-page.component';
 
-import { AuthGuard } from '../services/guards/auth.guard';
-import { RoutePath } from '../enums';
 import { TeacherMainPageComponent } from '../components/teacher/teacher-main-page/teacher-main-page.component';
 import { AddTeacherMainPageComponent } from '../components/teacher/add-teacher-page/add-teacher-page.component';
 import { TeacherDetailsPageComponent } from '../components/teacher/teacher-details-page/teacher-details-page.component';
+
+import { ProfessionalFieldMainPageComponent } from '../components/professional-field/professional-field-main-page/professional-field-main-page.component';
+import { PhdProgramMainPageComponent } from '../components/phd-program/phd-program-main-page/phd-program-main-page.component';
 
 const routes: Routes = [
     { path: RoutePath.login, component: LoginPageComponent },
